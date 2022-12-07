38735b4b609865e7cda17944ba7f6e430391fe4b
IvanKahinga <ikahinga@gmail.com>
2022-12-07 18:00:53 +0300

    Completed assignment Scope and var keyword

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  return  customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+function setBestCustomer() {
+   return bestCustomer = "not bob";
+}
+function overwriteBestCustomer(name) {
+  return  bestCustomer = name;
+}
+
+
+const leastFavoriteCustomer = "Harry"
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = "billy";
+}
+
+console.log(changeLeastFavoriteCustomer());
